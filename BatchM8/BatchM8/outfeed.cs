6e1ee478fea87159116b1f89f81fac3cd57f6f9f
sevenfloorsdown <sevenfloorsdown@gmail.com>
2017-03-25 19:28:47 +1100

    created outfeed class and associated tests

---
+++ BatchM8/BatchM8/outfeed.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Copyright 2017 Christian Rivera
+ * 
+ */
+
+using System;
+
+namespace sevenfloorsdown
+{
+    public class OutFeed
+    {
+        public SerialPortManager Port { get; set; }
+        public string Header { get; set; }
+        public string Footer { get; set; }
+        public string InfeedNum { get; set; }
+        public string FixedAsciiData { get; set; }
+        public int InputPLULength { get; set; }
+        public int InputPPKLength { get; set; }
+        public int OutputPLULength { get; set; }
+        public int OutputPPKLength { get; set; }
+
+        public OutFeed(SerialSettings settings)
+        {
+            Port = new SerialPortManager(settings.PortName)
+            {
+                Settings = settings
+            };
+        }
+
+        public string CreateOutputMessage(string inputMessage)
+        {
+            string result = String.Empty;
+            string plu = String.Empty;
+            string ppk = String.Empty;
+            try
+            {
+                plu = inputMessage.Substring(InputPLULength - OutputPLULength, OutputPLULength);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(String.Format("Mismatch with input and output PLU lengths: {0} vs {1}, {2}", 
+                    InputPLULength.ToString(), OutputPLULength.ToString(), e.Message));
+            }
+            try
+            {
+                ppk = inputMessage.Substring(InputPLULength + 1, OutputPPKLength);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(String.Format("Mismatch with input and output PPK lengths: {0} vs {1}, {2}",
+                    InputPPKLength.ToString(), OutputPPKLength.ToString(), e.Message));
+            }
+            result = InfeedNum + plu + ppk + FixedAsciiData;
+            return result;
+        }
+
+    }
+}
---
+++ BatchM8/BatchM8UnitTests/OutFeedUnitTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace sevenfloorsdown
+{
+    [TestClass]
+    public class OutFeedUnitTest
+    {
+        OutFeed DUT;
+        int inputPLULen = 6;
+        int inputPPKLen = 5;
+        string inplu = "061012";
+        string inppk = "1700 ";
+
+        int outputPLULen = 5;
+        int outputPPKLen = 4;
+        string _outheader = "\x02"; // STX
+        string _outfooter = "\x03"; // ETX
+        string outplu = "61012";
+        string outppk = "1700";
+        string fixeddata = "abcdef";
+        string inputMessage;
+        string expectedData;
+
+        private void InitDut()
+        {
+            inputMessage = inplu + "," + inppk;
+            SerialSettings tmpSettings = new SerialSettings()
+            {
+                PortName = "COM5"
+            };
+            DUT = new OutFeed(tmpSettings)
+            {
+                Header = _outheader,
+                Footer = _outfooter,
+                InputPLULength = inputPLULen,
+                InputPPKLength = inputPPKLen,
+                OutputPLULength = outputPLULen,
+                OutputPPKLength = outputPPKLen,
+                FixedAsciiData = fixeddata
+            };
+            expectedData = outplu + outppk + fixeddata;
+        }
+
+
+        [TestMethod]
+        public void SimpleOutputTest1()
+        {
+            InitDut();
+            for (int i = 1; i < 10; i++)
+            {
+                DUT.InfeedNum = i.ToString();
+                expectedData = DUT.InfeedNum + outplu + outppk + fixeddata;
+                Assert.AreEqual(expectedData, DUT.CreateOutputMessage(inputMessage));
+            }
+        }
+
+        [TestMethod]
+        public void ExceptionOutputTest1()
+        {
+            InitDut();
+            DUT.InputPLULength = DUT.OutputPLULength - 2;        
+            DUT.InfeedNum = "1";
+            try
+            {
+                string tmp = DUT.CreateOutputMessage(inputMessage);
+                Assert.IsTrue(false);
+            } 
+            catch (Exception e)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod]
+        public void ExceptionOutputTest2()
+        {
+            InitDut();
+            DUT.InputPPKLength = DUT.OutputPPKLength - 2;
+            DUT.InfeedNum = "1";
+            try
+            {
+                string tmp = DUT.CreateOutputMessage(inputMessage);
+                Assert.IsTrue(false);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
